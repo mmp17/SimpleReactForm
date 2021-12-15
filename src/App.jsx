122b39b5b7611ce92c56c36d1adbda4f2098811c
mmp17 <mirzaevmuzaffar@yahoo.com>
2021-12-15 15:07:37 +0900

    State

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
 import React, {Component} from "react";
 import './App.css';
+import { characters } from "./data";
 
 
 import Table from "./Table";
 
 
 class App extends Component {
+    state = {
+        characters: characters
+    }
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+    
+        this.setState({
+            characters: characters.filter((character, i) => { 
+                return i !== index;
+            })
+        });
+    }
+
+
     render() {
-      const characters = [
-        {
-          name: 'Charlie',
-          job: 'Janitor',
-        },
-        {
-          name: 'Mac',
-          job: 'Bouncer',
-        },
-        {
-          name: 'Dee',
-          job: 'Aspring actress',
-        },
-        {
-          name: 'Dennis',
-          job: 'Bartender',
-        },
-      ]
-  
+        const { characters } = this.state;
+        
       return (
         <div className="container">
-          <Table characterData={characters} />
+          <Table characterData={characters} removeCharacter={this.removeCharacter}/>
         </div>
       )
     }
